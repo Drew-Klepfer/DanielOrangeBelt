b0145c4d6e1a4a2b516f5c992f95f800da7e15b1
Drew-Klepfer <klepfed@wwu.edu>
2023-06-02 17:52:27 -0700

    6/2 update

--- portfoilio/lesson2.js
+++ portfoilio/lesson2.js
@@ -6,7 +6,7 @@ function startGame() {
     /* Here we will test our html button that calls this function by using console.log */
 
     /* create our array of words for the user to guess */
-    var words = ['rubik', 'coding', 'pizza', 'orange', 'yoshi'];
+    var words = ['rubik', 'coding', 'pizza', 'orange', 'yoshi', 'mario', 'antidisestablishmentarianism', 'pneumonoultramicroscopicsilicovolcanoconiosis', 'hippopotomonstrosesquippedaliophobia', 'floccinaucinihilipilification'];
 
     /* give an alert to welcome our user to the game */
     alert("Welcome to my hangman game \n Hangman is a word guessing game \n The computer will choose a random word for you to guess \n good luck");
@@ -23,11 +23,11 @@ function startGame() {
     }
 
     /* declare a variable to keep track of how many letters are left tin the secret word */
-    var lettersLeft = randomWord.length;
+    var lettersGuessed = randomWord.length;
 
     /* start a while loop that will run until until the user has guessed all the letters */
     /* HAD WRONG VARIABLE IN WHILE LOOP */
-    while (lettersLeft > 0) {
+    while (lettersGuessed > 0) {
         console.log("Hi")
         alert(answer.join(" "));
 
@@ -45,6 +45,10 @@ function startGame() {
             alert('Hey! Guess a letter');
         }
 
+        //switch(){
+
+        //}
+
         /* if the guess is valid */
         else{
             var underCaseGuess = guess.toLowerCase();
@@ -59,5 +63,5 @@ function startGame() {
     }
     /* show the user the found word with an alert */
     alert(answer.join(" "))
-    alert("Great job! The secret word was" + randomWord)
+    alert("Great job! The secret word was " + randomWord + ".")
 }
